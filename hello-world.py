4db45578ec15923c7cac49ad9b64fce1565e0d20
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-01-03 15:39:20 +0200

    Text no longer goes out of screen. It bounces instead.

--- hello-world.py
+++ hello-world.py
@@ -12,13 +12,17 @@ screen = pygame.display.set_mode(windowSize)
 myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
 
 helloWorld = myriadProFont.render("Hello World!", 1, (255, 0, 0), (255, 255, 255))
+helloWorldSize = helloWorld.get_size()
 
 x, y = 0, 0
+direction = (1, 1)
 
 clock = pygame.time.Clock()
 
 # The game loop
 while 1:
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -27,9 +31,13 @@ while 1:
 
     screen.blit(helloWorld, (x, y))
 
-    x = x + 1 if x < 800 else random.randint(0, 800)
-    y = y + 1 if y < 600 else random.randint(0, 600)
+    x += 5 * direction[0]
+    y += 5 * direction[1]
+
+    if x + helloWorldSize[0] > windowSize[0] or x < 0:
+        direction = (-1 * direction[0], direction[1])
+
+    if y + helloWorldSize[1] > windowSize[1] or y < 0:
+        direction = (direction[0], -1 * direction[1])
 
     pygame.display.update()
-
-# pygame.
